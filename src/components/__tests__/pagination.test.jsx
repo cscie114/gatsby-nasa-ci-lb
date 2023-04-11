798a6ff3185f67a8ba0d376c8782c5c03236c0d7
Arthur Barrett <abarrett@fas.harvard.edu>
2023-04-11 12:53:01 -0400

    Added jest for unit testing.

---
+++ src/components/__tests__/pagination.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Pagination from '../pagination';
+
+test('renders the pagination component', () => {
+  const { debug } = render(<Pagination currentPage={2} numPages={3} pathPrefix="/test" />);
+  debug();
+  expect(screen.queryAllByRole('link')).toHaveLength(5)
+});
