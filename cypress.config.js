a558f1978bb3ca9c61495d2105ee1570ae9293ea
Harvard-DCE-CSCIE3 <lbouthillier@fas.harvard.edu>
2023-04-18 17:31:13 -0700

    force deploy

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     // https://docs.cypress.io/guides/references/configuration#Testing-Type-Specific-Options
     // URL used as prefix for cy.visit() or cy.request() command's URL.
-    baseUrl: 'http://localhost:9000',
+    baseUrl: 'http://localhost:8000',
   },
 });
